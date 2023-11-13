2c96eadc231b560edc3890e1c38f3cffaecd00aa
Yang_Geonu <113342888+YangGeoun@users.noreply.github.com>
2023-11-13 20:32:47 +0900

    Update settings.py

--- sample/settings.py
+++ sample/settings.py
@@ -81,12 +81,8 @@ WSGI_APPLICATION = 'sample.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # mysqlclient librarly 설치
-        'NAME': 'mariadb',
-        'USER': 'root',
-        'PASSWORD': 'ssafy2365', # mariaDB 설치 시 입력한 root 비밀번호 입력
-        'HOST': 'svc.sel5.cloudtype.app',
-        'PORT': '30507'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
